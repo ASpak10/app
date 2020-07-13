8fd049543d76bbc7f309777110cd4cdfa69b5c68
Chakri Thongtae <chakri@192.168.1.39>
2020-07-14 00:09:11 +0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Route, BrowserRouter, Switch, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>
-      <p>React</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
