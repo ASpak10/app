0556bea00f4a275db0cc843ebf22572b95e946e6
Chakri Thongtae <chakri@MacBook-Pro-2.local>
2020-07-14 09:51:55 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route, BrowserRouter, Switch, Link } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <nav>
           <ul>
@@ -18,11 +19,12 @@ const App = () => {
           </ul>
         </nav>
         <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact component={NotFound} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 };
 
